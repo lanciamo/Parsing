e56df5b4a9bb4ee870a0b50669fff0a0f2b4d4cd
Leonid Kashin <lkashin@gmail.com>
2019-11-24 18:34:39 +0400

    Lesson 02 and 03

--- HWLesson02-03.py
+++ HWLesson02-03.py
@@ -1,14 +1,58 @@
 import re
+import requests
 import time
 
-import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 from pymongo import MongoClient
 from requests.exceptions import HTTPError
-from user_agent import generate_user_agent
+
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 ' \
+             'Safari/537.36 '
 
 
-def format_data(name, link, salary, site):
+def parse_hh(vacancy, pages, collection, mode="update"):
+    search_url = "https://hh.ru/search/vacancy"
+
+    for page in range(pages):
+
+        time.sleep(1)
+
+        params = {"text": f"{vacancy}", "page": f"{page}"}
+
+        html = make_request(search_url, params=params)
+
+        if html is None:
+            continue
+
+        soup = bs(html, 'lxml')
+        job_list = soup.find_all("div", class_="vacancy-serp-item")
+        for job in job_list:
+            link = job.find('a').get('href', 'no link')
+            span = job.find_all("span")
+            name = span[0].text
+            job_more = job.find_all('a')[1].text + ' ' + span[2].text + ' ' + span[5].text
+            salary_div = job.find("div", class_="vacancy-serp-item__compensation")
+            salary = "З/П не указана"
+            if salary_div:
+                salary = salary_div.get_text()
+
+            if mode == "update":
+                if not collection.find_one({"link": link}):
+                    collection.insert_one(format_data(name, link, salary, job_more, "hh.ru"))
+            else:
+                collection.insert_one(format_data(name, link, salary, job_more, "hh.ru"))
+
+
+def make_request(url, params):
+    try:
+        resp = requests.get(url, params=params, headers={"User-Agent": f'{user_agent}'})
+        resp.raise_for_status()
+        return resp.text
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err} while parsing (maybe number of pages was too big)')
+
+
+def format_data(name, link, salary, info, site):
     min_max = False
     if "—" in salary:
         min_s, max_s = salary.split("—")
@@ -24,53 +68,12 @@ def format_data(name, link, salary, site):
         max_s = "-"
         min_max = True
     else:
-        return {"name": name, "link": link, "salary": salary, "site": site}
-
+        return {"name": name, "info": info, "link": link, "salary": salary, "site": site}
     if min_max:
-        return {"name": name, "link": link, "min_salary": min_s, "max_salary": max_s, "site": site}
+        return {"name": name, "info": info, "link": link, "min_salary": min_s, "max_salary": max_s, "site": site}
 
 
-def make_request(url, params):
-    try:
-        resp = requests.get(url, params=params, headers={"User-Agent": generate_user_agent()})
-        resp.raise_for_status()
-        return resp.text
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err} while parsing hh.ru (maybe number of pages was too big)')
-
-
-def parse_hh(vacancy, pages, collection, mode="unique"):
-    search_url = "https://hh.ru/search/vacancy"
-
-    for page in range(pages):
-
-        time.sleep(1)
-
-        params = {"text": f"{vacancy}", "page": f"{page}"}
-
-        html = make_request(search_url, params=params)
-
-        if html is None:
-            continue
-
-        soup = BeautifulSoup(html, 'html.parser')
-        job_list = soup.find_all("div", class_="vacancy-serp-item")
-        for job in job_list:
-            link = job.find('a').get('href', 'no link')
-            name = job.find('a').get_text()
-            salary_div = job.find("div", class_="vacancy-serp-item__compensation")
-            salary = "З/П не указана"
-            if salary_div:
-                salary = salary_div.get_text()
-
-            if mode == "unique":
-                if not collection.find_one({"link": link}):
-                    collection.insert_one(format_data(name, link, salary, "hh.ru"))
-            else:
-                collection.insert_one(format_data(name, link, salary, "hh.ru"))
-
-
-def parse_superjob(vacancy, pages, collection, mode="unique"):
+def parse_superjob(vacancy, pages, collection, mode="update"):
     base_url = "https://www.superjob.ru"
     search_url = "https://www.superjob.ru/vacancy/search"
 
@@ -85,7 +88,7 @@ def parse_superjob(vacancy, pages, collection, mode="unique"):
         if html is None:
             continue
 
-        soup = BeautifulSoup(html, 'html.parser')
+        soup = bs(html, 'lxml')
         for job in soup.find_all("div", class_="f-test-vacancy-item"):
             links = job.find_all("a")
             vac_link = "No link"
@@ -95,48 +98,54 @@ def parse_superjob(vacancy, pages, collection, mode="unique"):
                     break
 
             job_html = make_request(base_url + vac_link, params={})
-            job_soup = BeautifulSoup(job_html, 'html.parser')
+            job_soup = bs(job_html, 'lxml')
             job_info = job_soup.find("div", class_="_3MVeX")
-
+            job_more = job_info.find("span", class_="_3mfro _1hP6a _2JVkc").getText()
             name = job_info.find("h1").get_text()
             salary = job_info.find("span", class_="_3mfro _2Wp8I ZON4b PlM3e _2JVkc").get_text()
 
-            if mode == "unique":
+            if mode == "update":
                 if not collection.find_one({"link": base_url + vac_link}):
-                    collection.insert_one(format_data(name, base_url + vac_link, salary, "superjob.ru"))
+                    collection.insert_one(format_data(name, base_url + vac_link, salary, job_more, "superjob.ru"))
             else:
-                collection.insert_one(format_data(name, base_url + vac_link, salary, "superjob.ru"))
+                collection.insert_one(format_data(name, base_url + vac_link, salary, job_more, "superjob.ru"))
 
 
 def search_job(salary, collection):
     for job in collection.find({"min_salary": {"$gt": salary}}):
         print(
-            "-" * 79 + f"\n{job['name']} ({job['link']})\nMin salary: {job['min_salary']}\nMax salary: {job['max_salary']}\nSite: {job['site']}")
+            "-" * 79 + f"\n{job['name']} ({job['link']})\nMin salary: {job['min_salary']}\nAdditional: {job['info']}\nMax salary: {job['max_salary']}\nSite: {job['site']}")
 
 
 def main():
     action = input(
-        "Что вы хотите сделать? Нажмите 1 чтобы сохранить вакансии, 2 чтобы сохранить вакансии без повторений, "
-        "3 чтобы вывести вакансии с З/П больше указанной: ")
+        "And what do you want?\n Choose:\n[1] Parsing everything all over again\n[2] Only update changes and "
+        "additions\n "
+        "[3] Show positions with a salary above a given \n[4] Drop current DataBase\n[5] Exit\n")
 
     client = MongoClient('localhost', 27017)
     db = client.jobs_database
     collection = db.jobs
 
     if action == "1":
-        position = input("Введите должность: ")  # python
-        page_num = int(input("Введите кол-во страниц: "))  # 2
+        position = input("Input position your prefer: ")  # python
+        page_num = int(input("For how many pages do you want go deeper : "))  # 2
         parse_hh(position, page_num, collection, mode="all")
         parse_superjob(position, page_num, collection, mode="all")
     elif action == "2":
-        position = input("Введите должность: ")  # python
-        page_num = int(input("Введите кол-во страниц: "))  # 2
-        parse_hh(position, page_num, collection, mode="unique")
-        parse_superjob(position, page_num, collection, mode="unique")
+        position = input("Input position your prefer: ")  # python
+        page_num = int(input("For how many pages do you want go deeper : "))
+        parse_hh(position, page_num, collection, mode="update")
+        parse_superjob(position, page_num, collection, mode="update")
     elif action == "3":
-        salary = float(input("Введите зарплату(в рублях): "))  # допустим
+        salary = float(input("From what salary will I look up?: "))
         search_job(salary, collection)
+    elif action == "4":
+        collection.drop()
+    elif action == "5":
+        return False
 
 
-if __name__ == "__main__":
-    main()
+while __name__ == "__main__":
+    if not main():
+        break
