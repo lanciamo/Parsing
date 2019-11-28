ba2aa936e8f30c8636c589be31bdfb1b5bf15eb4
Leonid Kashin <lkashin@gmail.com>
2019-11-28 13:12:38 +0400

    HW05 Scrapy Initial

---
+++ HW05/jobparser/spiders/sjru.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class SjruSpider(scrapy.Spider):
+    name = 'sjru'
+    allowed_domains = ['superjob.ru']
+    start_urls = ['http://superjob.ru/']
+
+    def parse(self, response):
+        pass
